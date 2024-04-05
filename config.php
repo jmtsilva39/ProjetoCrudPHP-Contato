2e4fee7f2dbccfa0bd7eadf582be1923c7100a1b
Jonez de Mello Toffani da Silva <jmts39@hotmail.com>
2024-04-05 10:43:14 -0300

    ok
    ok

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+//configuration file
+
+define('MYSQL_CONFIG', [
+    'host' => 'localhost',
+    'database' => 'php_pdo_contactos',
+    'username' => 'user_php_pdo_contactos',
+    'password' => '123456',
+
+]);
---
+++ eliminar_contacto.php
@@ -0,0 +1,53 @@
+<?php
+
+use sys4soft\Database;
+
+require_once('header.php');
+
+//check if there is a id in the url (query string)
+
+if (empty($_GET['id'])) {
+    header('Location: index.php');
+}
+
+// include files
+require_once('config.php');
+require_once('libraries/Database.php');
+
+// get contatos informations
+$id = $_GET['id'];
+$database = new Database(MYSQL_CONFIG);
+$params = [
+    ':id' => $id
+];
+
+//check if the delete answer was given
+if (empty($_GET['delete'])) {
+    $results = $database->execute_query("SELECT * FROM contato WHERE id = :id", $params);
+    $contato = $results->results[0];
+} else {
+    $database->execute_non_query("DELETE FROM contato WHERE id = :id", $params);
+    header('Location: index.php');
+}
+
+?>
+
+<div class="row">
+    <div class="col text-center">
+        <h3>Deseja eliminar o seguinte contacto?</h3>
+
+        <div class="my-4">
+            <div>
+                <span class="me-5"><strong> <?= $contato->nome ?> </strong></span>
+                <span>Telefone: <strong> <?= $contato->telefone ?> </strong></span>
+            </div>
+        </div>
+
+        <a href="index.php" class="btn btn-outline-dark yes-no-width">Não</a>
+        <a href="eliminar_contacto.php?id=<?= $id ?>?delete=yes" class="btn btn-outline-dark yes-no-width">Sim</a>
+    </div>
+</div>
+
+<?php
+require_once('footer.php');
+?>
